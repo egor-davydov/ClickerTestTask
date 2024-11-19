6ef7c8fa761d7c104c4c3a82c2dba9835c4b3ba3
Egor Davydov <a.egordavidov@gmail.com>
2024-11-19 18:23:35 +0300

    import fluxy

---
+++ Assets/Plugins/FluXY/Scripts/TargetProviders/FluxyTargetProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fluxy
+{
+    public interface IFluxyTargetProvider
+    {
+        List<FluxyTarget> GetTargets();
+    }
+
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(FluxyContainer))]
+    public abstract class FluxyTargetProvider : MonoBehaviour, IFluxyTargetProvider
+    {
+        public abstract List<FluxyTarget> GetTargets();
+    }
+}
